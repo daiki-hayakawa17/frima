3a992e6da6c51bb6ebdef7e9f655492eaf73fc2c
daiki hayakawa <moko021917@outlook.jp>
2025-11-24 16:28:44 +0900

    新規通知機能追加#117

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -13,6 +13,11 @@
     <a class="profile__link" href="/mypage/profile">プロフィールを編集</a>
 </div>
 <div class="list__nav" id="list__nav">
+    @php
+        $totalUnread = $items->sum(function ($item) {
+            return $item->room ? $item->room->messages->count() : 0;
+        });
+    @endphp
     <a href="{{ route('mypage', ['mypage' => 'sell']) }}" class="list__nav--text {{ request('mypage', 'sell') === 'sell' ? 'active' : '' }}">
         出品した商品
     </a>
@@ -22,15 +27,24 @@
     <a href="{{ route('mypage', ['mypage' => 'trading']) }}" class="list__nav--text trading {{ request('mypage') === 'trading' ? 'active' : '' }}">
         取引中の商品
     </a>
+    @if ($totalUnread > 0)
+        <p class="total__unread">{{ $totalUnread }}</p>
+    @endif
 </div>
 <div class="line"></div>
 <div class="item__contents">
     @if (!empty($items))
     <div class="item__contents--inner">
         @foreach ($items as $item)
-            @if ($item->status !== 'trading')
+            @if ($item->status === 'trading' && request('mypage') === 'trading')
             <div class="item__content">
-                <a href="/item/{{$item->id}}" class="item__link">
+                @php
+                    $unread = $item->room ? $item->room->messages->count() : 0;
+                @endphp
+                <a href="/trading/chat/{{$item->id}}" class="item__link">
+                    @if ($unread > 0)
+                        <span class="badge__red">{{ $unread }}</span>
+                    @endif
                     <img src="{{ asset($item->image) }}" alt="{{ $item->name }}の画像" class="img__content">
                     <div class="detail__content">
                         <p>{{$item->name}}</p>
@@ -39,7 +53,7 @@
             </div>
             @else
             <div class="item__content">
-                <a href="/trading/chat/{{$item->id}}" class="item__link">
+                <a href="/item/{{$item->id}}" class="item__link">
                     <img src="{{ asset($item->image) }}" alt="{{ $item->name }}の画像" class="img__content">
                     <div class="detail__content">
                         <p>{{$item->name}}</p>
